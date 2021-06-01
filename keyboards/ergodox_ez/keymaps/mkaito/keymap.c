6cb21144a46c2afe7f320d30005b4f2ac60e2527
Christian Höppner <chris@mkaito.net>
2021-06-01 22:58:34 +0100

    mkaito update

--- keyboards/ergodox_ez/keymaps/mkaito/keymap.c
+++ keyboards/ergodox_ez/keymaps/mkaito/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT(KC_INS),  KC_Y,    KC_U,     KC_I,    KC_O,       KC_P,        KC_MINUS,
                        KC_H,    KC_J,     KC_K,    KC_L,       KC_SCLN,     KC_QUOTE,
         KC_EQL,        KC_N,    KC_M,     KC_COMM, KC_DOT,     KC_SLSH,     KC_RSFT,
-                                MO(SYMB), KC_LALT, LALT(KC_P), LALT(KC_F8), LALT(KC_F1),
+                                MO(SYMB), KC_RALT, LALT(KC_P), LALT(KC_F8), LALT(KC_F1),
 
                        KC_HOME, KC_END,
                        KC_PGUP,
